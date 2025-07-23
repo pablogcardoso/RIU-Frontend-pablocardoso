cc7bd0f0419f84d9f439529fbe06db02c63fbfb6
pablo-cardoso <pablo.cardoso@fluxit.com.ar>
2025-07-22 22:36:56 -0300

    feature RIU-1 add material angular and main layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { SharedModule } from './shared/shared.module';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [SharedModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'riu-hero-challenge';
+export class AppComponent  {
+  title = 'Heroes';
+
 }
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
+import { MatCardModule } from '@angular/material/card';
+
+@NgModule({
+    imports: [
+        MatToolbarModule,
+        MatButtonModule,
+        MatIconModule,
+        MatTableModule,
+        MatSortModule,
+        MatCardModule],
+    exports: [
+        MatToolbarModule,
+        MatButtonModule,
+        MatIconModule,
+        MatTableModule,
+        MatSortModule,
+        MatCardModule],
+    declarations: [],
+    providers: [],
+})
+export class SharedModule { }
